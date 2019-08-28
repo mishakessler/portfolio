4c618bfa51cd4ee365aadb808c16bfb1a79094c7
Misha Kessler <misha.kessler@gmail.com>
2019-08-28 11:41:20 -0400

    Install React Router DOM, test React Spring animations.

--- misha-kessler/src/App.js
+++ misha-kessler/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+// React
+import React, { Component } from 'react'
+
+// React Router
+import {
+  Link,
+  Route,
+  withRouter,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+// React Semantic
+import {
+  Icon,
+  Button,
+} from 'semantic-ui-react'
+
+// Pages
+import Landing from './pages/Landing'
+
+// Components
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="app">
-
+      <Landing />
     </div>
   );
 }
