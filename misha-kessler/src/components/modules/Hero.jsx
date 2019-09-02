bf47f7ac82f623dcdb11cf134bb03d26a6d1b975
Misha Kessler <misha.kessler@gmail.com>
2019-09-02 15:56:39 -0400

    Import and refactor lettermark; refactor enter CTAs with Router Link to for transition effect.

--- misha-kessler/src/components/modules/Hero.jsx
+++ misha-kessler/src/components/modules/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero(props) {
   return (
     <div className="hero">
       <h1>{props.title}</h1>
-      <img src={props.image} />
+      {/* <img src={props.image} /> */}
       <h3>{props.tagline}</h3>
       <p>{props.description}</p>
       <p>{props.helper}</p>
--- misha-kessler/src/components/roles/Advocate.jsx
+++ misha-kessler/src/components/roles/Advocate.jsx
@@ -1,14 +1,26 @@
 // React
 import React from 'react'
 
+// React Router
+import {
+  Link
+} from 'react-router-dom'
+
+
 // React Semantic
 import {
   Button,
 } from 'semantic-ui-react'
 
+// Assets 
+import Lettermark from '../../assets/graphics/lettermark-white.png'
+
 export default function Advocate() {
   return (
     <div className="landing-advocate">
+      <div className="landing-lettermark">
+        <img src={Lettermark} />
+      </div>
       <div className="landing-titles">
         <h5>Advocate</h5>
         <span className="hide-on-mobile"><h5>|</h5></span>
@@ -25,17 +37,18 @@ export default function Advocate() {
             <span className="hide-on-desktop">swipe</span><span className="hide-on-mobile">click & drag</span> right for entrepreneurship</h6>
         </div>
         <div className="enter-cta">
-          <Button
-            basic
-            inverted
-            content='View Advocacy'
-            href='/advocacy'
-            labelPosition='right'
-            className='smooth'
-            icon={{
-              name: 'chevron down',
-            }}
-          />
+          <Link to="/advocacy" className="smooth">
+            <Button
+              basic
+              inverted
+              content='View Advocacy'
+              labelPosition='right'
+              className='smooth'
+              icon={{
+                name: 'chevron down',
+              }}
+            />
+          </Link>
         </div>
         <div className="right-cta">
           <h6 className="smooth"></h6>
