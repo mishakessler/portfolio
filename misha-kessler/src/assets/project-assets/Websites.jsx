0f6c41ac2d18d8cdb600698717a5abdc0faaf99a
Misha Kessler <misha.kessler@gmail.com>
2019-09-06 09:18:18 -0400

    Add assets to projects.

--- misha-kessler/src/assets/project-assets/Websites.jsx
+++ misha-kessler/src/assets/project-assets/Websites.jsx
@@ -1,6 +1,42 @@
 const Websites = [
   {
-    id: 2,
+    id: ,
+    featured: true,
+
+    name: 'Misha Kessler',
+    date: 'Sep 2019',
+    meta: 'React',
+    tagline: 'A portfolio site for Misha Kessler following his stint at General Assembly, built on React with multiple React libraries.',
+
+    category: 'Front End', // Concept, Front End, Full Stack
+    progress: 'Live', // R&D, MVP, Post-MVP, Private Beta, Live
+
+    url_to_img: 'https://imgur.com/ygoBsKu.png',
+    url_to_demo: '',
+    url_to_github: 'https://github.com/mishakessler/portfolio',
+    url_to_casestudy: '/projects/crisis-international',
+
+    markdown: true,
+    wordpress: false,
+    html: true,
+    css: true,
+    php: false,
+    javascript: true,
+    jquery: false,
+    nodejs: true,
+    expressjs: false,
+    reactjs: true,
+    mysql: false,
+    postgresql: true,
+    ruby: false,
+    rails: true,
+    auth: true,
+    adobe: true,
+    responsive: true,
+    accessible: true,
+  },
+  {
+    id: ,
     featured: true,
 
     name: 'Crisis International Enterprise',
@@ -27,7 +63,7 @@ const Websites = [
     expressjs: false,
     reactjs: true,
     mysql: false,
-    postgresql: false,
+    postgresql: true,
     ruby: false,
     rails: true,
     auth: true,
@@ -36,7 +72,7 @@ const Websites = [
     accessible: true,
   },
   {
-    id: 1,
+    id: ,
     featured: true,
 
     name: 'Crisis International',
@@ -71,6 +107,117 @@ const Websites = [
     responsive: true,
     accessible: true,
   },
+  {
+    id: ,
+    featured: false,
+
+    name: 'Barrel-Aged',
+    date: 'Jul 2019',
+    meta: 'React & Express',
+    tagline: 'The deliverable from a 4-person group-build, a full-stack forum and review site for whiskey lovers, utilizing React, Express, Bcrypt, JSON Web Token, and Git versioning.',
+
+    category: 'Full Stack', // Concept, Front End, Full Stack
+    progress: 'MVP', // R&D, MVP, Post-MVP, Private Beta, Live
+
+    url_to_img: 'https://imgur.com/jDqHUbW.png',
+    url_to_demo: 'http://www.barrel-aged.surge.sh',
+    url_to_github: 'https://github.com/mishakessler/barrel-aged',
+    url_to_casestudy: '',
+
+    markdown: true,
+    wordpress: false,
+    html: true,
+    css: true,
+    php: false,
+    javascript: true,
+    jquery: false,
+    nodejs: true,
+    expressjs: true,
+    reactjs: true,
+    mysql: false,
+    postgresql: true,
+    ruby: false,
+    rails: true,
+    auth: true,
+    adobe: true,
+    responsive: true,
+    accessible: true,
+  },
+  {
+    id: ,
+    featured: false,
+
+    name: '',
+    date: '',
+    meta: '',
+    tagline: 'A full stack B2B resource directory and knowledge base for social service agencies and related fields that provides comprehensive, filterable, searchable resources and articles.',
+
+    category: '', // Concept, Front End, Full Stack
+    progress: '', // R&D, MVP, Post-MVP, Private Beta, Live
+
+    url_to_img: '',
+    url_to_demo: '',
+    url_to_github: '',
+    url_to_casestudy: '',
+
+    markdown: false,
+    wordpress: false,
+    html: false,
+    css: false,
+    php: false,
+    javascript: false,
+    jquery: false,
+    nodejs: false,
+    expressjs: false,
+    reactjs: false,
+    mysql: false,
+    postgresql: false,
+    ruby: false,
+    rails: false,
+    auth: false,
+    adobe: false,
+    responsive: false,
+    accessible: false,
+  },
 ]
 
 export default Websites
+
+
+
+// {
+//   id: ,
+//   featured: false,
+
+//   name: '',
+//   date: '',
+//   meta: '',
+//   tagline: 'A full stack B2B resource directory and knowledge base for social service agencies and related fields that provides comprehensive, filterable, searchable resources and articles.',
+
+//   category: '', // Concept, Front End, Full Stack
+//   progress: '', // R&D, MVP, Post-MVP, Private Beta, Live
+
+//   url_to_img: '',
+//   url_to_demo: '',
+//   url_to_github: '',
+//   url_to_casestudy: '',
+
+//   markdown: false,
+//   wordpress: false,
+//   html: false,
+//   css: false,
+//   php: false,
+//   javascript: false,
+//   jquery: false,
+//   nodejs: false,
+//   expressjs: false,
+//   reactjs: false,
+//   mysql: false,
+//   postgresql: false,
+//   ruby: false,
+//   rails: false,
+//   auth: false,
+//   adobe: false,
+//   responsive: false,
+//   accessible: false,
+// },
