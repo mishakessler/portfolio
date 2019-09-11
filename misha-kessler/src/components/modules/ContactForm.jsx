e7e210814ff529cf2cf3ca0226fb4c04a9ba4b9b
Misha Kessler <misha.kessler@gmail.com>
2019-09-11 15:23:00 -0400

    Update contact form for stateful component via Semantic.

--- misha-kessler/src/components/modules/ContactForm.jsx
+++ misha-kessler/src/components/modules/ContactForm.jsx
@@ -10,48 +10,70 @@ import {
 } from 'semantic-ui-react'
 
 export default class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "", subject: "", message: "" };
+  }
+
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state })
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
+    const { name, email, message, subject } = this.state;
     return (
       <div>
         <Form
-          netlify
-          name='contact-form'>
+          error
+          success
+          name='contact-form'
+          onSubmit={this.handleSubmit}>
+          <input type="hidden" name="form-name" value="contact" />
           <Form.Group
             widths='equal'>
             <Form.Field
-              name='first-name'
-              id='first-name'
+              name='name'
+              id='name'
+              value={name}
               control={Input}
-              placeholder='First Name'
+              placeholder='Name'
+              onChange={this.handleChange}
             />
-            <Form.Field
-              name='last-name'
-              id='last-name'
-              control={Input}
-              placeholder='Last Name'
-            />
-          </Form.Group>
-          <Form.Group
-            widths='equal'>
             <Form.Field
               name='email'
               id='email'
+              value={email}
               control={Input}
               type='email'
               placeholder='Email Address'
+              onChange={this.handleChange}
             />
             <Form.Field
               name='subject'
               id='subject'
+              value={subject}
               control={Input}
               placeholder='Subject'
+              onChange={this.handleChange}
             />
           </Form.Group>
           <Form.Field
             name='message'
             id='message'
+            value={message}
             control={TextArea}
             placeholder='Message'
+            onChange={this.handleChange}
           />
           <Form.Field
             id='submit'
